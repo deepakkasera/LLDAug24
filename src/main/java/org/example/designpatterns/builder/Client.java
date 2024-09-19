2a96952299e446a5629023f7bd6cc37259d95ecf
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-09-19 23:11:18 +0530

    Builder-2

--- src/main/java/org/example/designpatterns/builder/Client.java
+++ src/main/java/org/example/designpatterns/builder/Client.java
@@ -3,12 +3,24 @@ package org.example.designpatterns.builder;
 public class Client {
     public static void main(String[] args) {
 //        Builder builder = new Builder();
-        Builder builder = Student.getBuilder();
-        builder.setName("Rajneesh");
-        builder.setAge(25);
-        builder.setPsp(95.0);
-        builder.setGradYear(2021);
 
-        Student student = builder.build();
+//        Builder builder = Student.getBuilder();
+//        builder.setName("Rajneesh");
+//        builder.setAge(25);
+//        builder.setPsp(95.0);
+//        builder.setGradYear(2021);
+//
+//        Student student = builder.build();
+
+
+        Student student = Student.getBuilder()
+                .setName("Rajneesh")
+                .setAge(25)
+                .setBatch("Aug23 Beginner")
+                .setGradYear(2021)
+                .setUnivName("Scaler")
+                .build();
+
+        System.out.println("DEBUG");
     }
 }
--- src/main/java/org/example/designpatterns/builder/Student.java
+++ src/main/java/org/example/designpatterns/builder/Student.java
@@ -1,7 +1,5 @@
 package org.example.designpatterns.builder;
 
-import java.util.SplittableRandom;
-
 public class Student {
     private String name;
     private int age;
@@ -11,7 +9,7 @@ public class Student {
     private String univName;
     private int rollNumber;
 
-    Student(Builder builder) {
+    private Student(Builder builder) {
         this.name = builder.getName();
         this.age = builder.getAge();
         this.psp = builder.getPsp();
@@ -19,5 +17,89 @@ public class Student {
 
     public static Builder getBuilder() {
         return new Builder();
+    }
+
+    //Inner Class.
+    static class Builder {
+        private String name;
+        private int age;
+        private double psp;
+        private String batch;
+        private int gradYear;
+        private String univName;
+        private int rollNumber;
+
+        public String getName() {
+            return name;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Builder setAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public double getPsp() {
+            return psp;
+        }
+
+        public Builder setPsp(double psp) {
+            this.psp = psp;
+            return this;
+        }
+
+        public String getBatch() {
+            return batch;
+        }
+
+        public Builder setBatch(String batch) {
+            this.batch = batch;
+            return this;
+        }
+
+        public int getGradYear() {
+            return gradYear;
+        }
+
+        public Builder setGradYear(int gradYear) {
+            this.gradYear = gradYear;
+            return this;
+        }
+
+        public String getUnivName() {
+            return univName;
+        }
+
+        public Builder setUnivName(String univName) {
+            this.univName = univName;
+            return this;
+        }
+
+        public int getRollNumber() {
+            return rollNumber;
+        }
+
+        public Builder setRollNumber(int rollNumber) {
+            this.rollNumber = rollNumber;
+            return this;
+        }
+
+        public Student build() {
+            //validations start
+            if (this.getGradYear() >= 2024) {
+                throw new RuntimeException("Grad year should be less than or equal to 2023.");
+            }
+            //validations end
+
+            return new Student(this);
+        }
     }
 }
